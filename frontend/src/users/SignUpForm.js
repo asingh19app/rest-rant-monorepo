e9acb86f8d4b8efbbda331bdb9f4f9a750a7320a
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2023-04-04 12:45:03 -0400

    added login and signup features

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -67,6 +67,15 @@ function SignUpForm() {
 							name="email"
 						/>
 					</div>
+					<div className="col-sm 6 form-group">
+						<label htmlFor="password">Password</label>
+						<input
+						type = "password"
+						required
+						value = {user.password}
+						onChange={e => setUser({...user, password: e.target.value})}
+						/>
+					</div>
 				</div>
 				<input className="btn btn-primary" type="submit" value="Sign Up" />
 			</form>
